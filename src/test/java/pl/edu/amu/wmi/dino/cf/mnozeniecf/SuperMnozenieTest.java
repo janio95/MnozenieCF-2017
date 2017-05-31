fa2b14293e96a8a6fd08b9798a06dc16ac5a8235
Mikołaj Stasiak <s416168@shell.wmi.amu.edu.pl>
2017-05-31 14:33:14 +0200

    fix

--- src/test/java/pl/edu/amu/wmi/dino/cf/mnozeniecf/SuperMnozenieTest.java
+++ src/test/java/pl/edu/amu/wmi/dino/cf/mnozeniecf/SuperMnozenieTest.java
@@ -110,4 +110,22 @@ public class SuperMnozenieTest {
         assertEquals("-0.625", SuperMnozenie.mul("-0.625000","1"));
     }
     
+    /*
+    Mikołaj Stasiak
+    */
+     @Test
+    public void testMS1()
+    {
+        assertEquals("0.00025", SuperMnozenie.mul("0.00025","1"));
+    }
+    @Test
+    public void testMS2()
+    {
+        assertEquals("0.00015707963267948965", SuperMnozenie.mul(""+Math.PI,"0.00005"));
+    }
+    @Test
+    public void testMS3()
+    {
+        assertEquals("0.02718281828459045", SuperMnozenie.mul("0.01",""+Math.E));
+    }
 }
